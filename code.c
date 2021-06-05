ffb25c3a62e03c497f9a9268e0756baf59b3a9df
YoussefHekal <85392431+YoussefHekal@users.noreply.github.com>
2021-06-05 12:05:32 +0200

    Update code.c

--- code.c
+++ code.c
@@ -1,15 +1,14 @@
-// to calculate the distance 
 
 
-void distance(){
+// to calculate the distance between 2 locations 
 
-Delayms(delay2);                //function to make delay(delay2 = 2000 ms defined in the header file) between  the readings of the GPS
+double haversine()
+{
+    double dlong = (longitude - previouslong) * d2r;// d2r is a constant to convert to radian defined in the header
+    double dlat = (latitude - previouslat) * d2r;
+    double a = pow(sin(dlat/2.0), 2) + cos(previouslat*d2r) * cos(latitude*d2r) * pow(sin(dlong/2.0), 2);
+    double c = 2 * atan2(sqrt(a), sqrt(1-a));
+    double d = R * c;		// R is the radius of the Earth Defined in the header file  
 
-ReadData();	                // to set the new latitude and longitude from the GPS
-
-Totaldistance += haversine();	// haversine is a function to calculate distance between 2 locations
-
-previouslat = latitude; 		
-previouslong = longitude;
-
+    return d * 1000;
 }
